39758c9073ff0c77ff0cd02b92bc3817455b754d
Michał Wielopolski <77637937+wielopolski@users.noreply.github.com>
2023-03-13 04:10:07 +0100

    initial commit

---
+++ app/app/Models/PhraseStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace app.Models
+{
+    public class PhraseStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string PhrasesCollectionName { get; set; } = null!;
+    }
+}
---
+++ app/app/Services/PhrasesService.cs
@@ -0,0 +1,39 @@
+﻿using app.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace app.Services
+{
+    public class PhrasesService
+    {
+        private readonly IMongoCollection<Phrase> _PhrasesCollection;
+
+        public PhrasesService(
+            IOptions<PhraseStoreDatabaseSettings> PhraseStoreDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                PhraseStoreDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                PhraseStoreDatabaseSettings.Value.DatabaseName);
+
+            _PhrasesCollection = mongoDatabase.GetCollection<Phrase>(
+                PhraseStoreDatabaseSettings.Value.PhrasesCollectionName);
+        }
+
+        public async Task<List<Phrase>> GetAsync() =>
+            await _PhrasesCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Phrase?> GetAsync(string id) =>
+            await _PhrasesCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Phrase newPhrase) =>
+            await _PhrasesCollection.InsertOneAsync(newPhrase);
+
+        public async Task UpdateAsync(string id, Phrase updatedPhrase) =>
+            await _PhrasesCollection.ReplaceOneAsync(x => x.Id == id, updatedPhrase);
+
+        public async Task RemoveAsync(string id) =>
+            await _PhrasesCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
