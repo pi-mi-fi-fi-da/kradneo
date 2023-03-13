274892c759f6dabd50f8b9f400145211d79c43a8
Michał Wielopolski <77637937+wielopolski@users.noreply.github.com>
2023-03-13 04:10:07 +0100

    initial commit

---
+++ app/app/Controllers/PhrasesController.cs
@@ -0,0 +1,97 @@
+﻿using app.Models;
+using app.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app.Controllers
+{
+    public class PhrasesController : Controller
+    {
+        private readonly PhrasesService _PhrasesService;
+
+        public PhrasesController(PhrasesService PhrasesService) =>
+            _PhrasesService = PhrasesService;
+
+        // GET: PhrasesController
+        public async Task<ActionResult<Phrase>> Index()
+        {
+            var phrases = await _PhrasesService.GetAsync();
+            if (phrases is null)
+            {
+                return NotFound();
+            }
+
+            return View(phrases);
+        }
+
+        // GET: PhrasesController/Details/5
+        public async Task<ActionResult<Phrase>> Details(string id)
+        {
+            var phrase = await _PhrasesService.GetAsync(id);
+            return View(phrase);
+        }
+
+        // GET: PhrasesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PhrasesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PhrasesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PhrasesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PhrasesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PhrasesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
