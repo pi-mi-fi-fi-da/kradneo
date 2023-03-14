07eed6903270e54cf55d08ce6286a9722f827c50
Michał Wielopolski <77637937+wielopolski@users.noreply.github.com>
2023-03-14 01:47:42 +0100

    small improvement, add setup for PhraseController

---
+++ app/app/Models/PhraseProductStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace app.Models
+{
+    public class PhraseProductStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string PhraseProductsCollectionName { get; set; } = null!;
+    }
+}
